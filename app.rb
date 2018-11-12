fd8f29293fce1d83fc92a76306ee769d111a4295
Jackwhitener <jackwhitener1@gmail.com>
2018-11-12 15:10:41 -0500

    Age post thingy. Probably have done something terribly wrong.

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+
+
+get '/' do
+	erb :name
+end
+post '/name' do
+	firstname = params[:f_name]
+	redirect '/lastname?f_name=' + firstname
+end
+get '/lastname' do
+	f_name = params[:f_name]
+ 	erb :lastname, locals: {f_name: f_name}
+end
+post '/last_name' do
+	firstname = params[:f_name]
+	lastname = params[:l_name]
+	redirect '/age?f_name=' + firstname + '&l_name=' + lastname
+end
+
+get '/age' do
+	last_name = params[:l_name]
+	firstname = params[:f_name]
+	erb :age, locals: {last_name: last_name, firstname: firstname}
+end
+post '/u_age' do
+	last_name = params[:l_name]
+	firstname = params[:f_name]
+	age = params[:u_age]
+	redirect '/favcolor?f_name=' + firstname + '&l_name' + lastname + '&u_age' + age
+end
---
+++ personal_details_test.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+require_relative "personal_details.rb"
+class My_test < Minitest::Test
+	def test1
+		assert_equal(1,1)
+	end
+end
